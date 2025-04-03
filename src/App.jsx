1712fab6f6a119ab35577807d94d8ff0e92f6c72
[Atakan Arıkan] <[atakanarkn1@outlook.com]>
2025-04-03 18:14:55 +0300

    cv eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -143,14 +143,14 @@ function Header({ i18n, t, scrollToSection, section1Ref, section2Ref, section3Re
           <li><a href="#" className="menu__item">About</a></li>
           <li><a href="#" className="menu__item">Project</a></li>
           <li><a href="#" className="menu__item">Contact</a></li>
-          <span><a href="#" className='button'>CV<i className="fa-solid fa-download"></i><svg>
+          <span><a href="/new-cv.pdf" download="new-cv.pdf" className='button'>CV<i className="fa-solid fa-download"></i><svg>
             <rect
               x="0" y="0"
               fill='none'
               width="100%"
               height="100%" />
           </svg></a></span>
-
+          
         </ul>
       </div>
 
@@ -182,7 +182,7 @@ function HeaderContent({ i18n, t }) {
               data-type='["Web Developer","Front-End Developer", "Freelancer"]'
             ></span>
           </h3>
-          <span><a href="#" className='button'>CV<i className="fa-solid fa-download"></i><svg>
+          <span><a href="/new-cv.pdf" download="Atakan Arıkan CV.pdf" className='button'>CV<i className="fa-solid fa-download"></i><svg>
             <rect
               x="0" y="0"
               fill='none'
